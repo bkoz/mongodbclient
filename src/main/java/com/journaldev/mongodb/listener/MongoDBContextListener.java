fcfb906205563c150ed02d6c3cd2effd5a81e1d7
Bob Kozdemba <bkozdemba@gmail.com>
2016-02-14 10:30:01 -0600

    Update MongoDBContextListener.java
    Changed service names to MONGODB_

--- src/main/java/com/journaldev/mongodb/listener/MongoDBContextListener.java
+++ src/main/java/com/journaldev/mongodb/listener/MongoDBContextListener.java
@@ -27,11 +27,11 @@ public class MongoDBContextListener implements ServletContextListener {
 			// The following variables are provided by the Kubernetes "mongod" service.
 			//
 			System.out.println("MongoClient init started.");
-			System.out.println("MONGOD_SERVICE_HOST = " + System.getenv("MONGOD_SERVICE_HOST"));
-			System.out.println("MONGOD_SERVICE_PORT = " + System.getenv("MONGOD_SERVICE_PORT"));
-			System.out.println("Integer.parseInt = " + Integer.parseInt(System.getenv("MONGOD_SERVICE_PORT")));
+			System.out.println("MONGODB_SERVICE_HOST = " + System.getenv("MONGODB_SERVICE_HOST"));
+			System.out.println("MONGODB_SERVICE_PORT = " + System.getenv("MONGODB_SERVICE_PORT"));
+			System.out.println("Integer.parseInt = " + Integer.parseInt(System.getenv("MONGODB_SERVICE_PORT")));
 			ServletContext ctx = sce.getServletContext();
-			MongoClient mongo = new MongoClient(System.getenv("MONGOD_SERVICE_HOST"), Integer.parseInt(System.getenv("MONGOD_SERVICE_PORT")));
+			MongoClient mongo = new MongoClient(System.getenv("MONGODB_SERVICE_HOST"), Integer.parseInt(System.getenv("MONGOD_SERVICE_PORT")));
 			// ctx.getInitParameter("MONGODB_HOST"), 
 			// System.getenv("MONGOD_SERVICE_HOST"),
 			// Integer.parseInt(ctx.getInitParameter("MONGODB_SERVICE_PORT")));

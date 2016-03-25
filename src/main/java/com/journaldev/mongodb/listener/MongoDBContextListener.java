684508ce80ca1eae1e7159aeb5d2036cc943c5cc
bkoz <bkozdemba@gmail.com>
2016-03-25 01:07:29 -0500

    Added mongo init check

--- src/main/java/com/journaldev/mongodb/listener/MongoDBContextListener.java
+++ src/main/java/com/journaldev/mongodb/listener/MongoDBContextListener.java
@@ -65,11 +65,8 @@ public class MongoDBContextListener implements ServletContextListener {
       }
       sce.getServletContext().setAttribute("MONGO_CLIENT", mongo);
     } catch (UnknownHostException e) {
-      e printStackTrace();
       throw new RuntimeException("MongoClient init failed");
-    } catch (MongoException e) {
-       e printStackTrace();
-    }
+    } 
   }
 
 }
